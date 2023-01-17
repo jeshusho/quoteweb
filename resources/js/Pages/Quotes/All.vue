cb3ae2dea2253472ddab136359bf9e2f0a482d3c
Jesus Angeles <jesus.angeles@gmail.com>
2023-01-17 09:54:48 -0300

    primer commit

--- resources/js/Pages/Quotes/All.vue
+++ resources/js/Pages/Quotes/All.vue
@@ -30,34 +30,34 @@ import Welcome from '@/Components/Welcome.vue';
                 <thead>
                 <tr>
                     <th></th>
-                    <th>Name</th>
-                    <th>Job</th>
-                    <th>Favorite Color</th>
+                    <th>Presupuesto</th>
+                    <th>Entrega prevista</th>
+                    <th>Estado</th>
                 </tr>
                 </thead>
-                <tbody>
-                <!-- row 1 -->
+                <!-- <tbody>
+
                 <tr>
                     <th>1</th>
                     <td>Cy Ganderton</td>
                     <td>Quality Control Specialist</td>
                     <td>Blue</td>
                 </tr>
-                <!-- row 2 -->
+
                 <tr>
                     <th>2</th>
                     <td>Hart Hagerty</td>
                     <td>Desktop Support Technician</td>
                     <td>Purple</td>
                 </tr>
-                <!-- row 3 -->
+
                 <tr>
                     <th>3</th>
                     <td>Brice Swyre</td>
                     <td>Tax Accountant</td>
                     <td>Red</td>
                 </tr>
-                </tbody>
+                </tbody> -->
             </table>
         </div>
     </AppLayout>
