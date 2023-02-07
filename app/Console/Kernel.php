d77b0a051d444df179f0679e995dd899021325e3
Jesus Angeles <jesus.angeles@gmail.com>
2023-02-07 20:04:50 -0300

    Modifica Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('update:census')->dailyAt('23:04');
+        $schedule->command('update:census')->dailyAt('23:05');
         //$schedule->call('App\Http\Controllers\PadronController@test')->everyMinute();
     }
 
