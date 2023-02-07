64e4fbb3d8d9b2c44bf1ce32a784b598633b0967
Jesus Angeles <jesus.angeles@gmail.com>
2023-02-07 19:22:17 -0300

    Add subfolders

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('update:census')->dailyAt('00:20');
+        $schedule->command('update:census')->dailyAt('00:24');
     }
 
     /**
